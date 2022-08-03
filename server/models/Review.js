0a9aae04ae8918ff9b17ba64168b8e88b054e6ab
tmenture <thomasoxemail@gmail.com>
2022-08-03 19:05:39 -0400

    started the server models

--- server/models/Review.js
+++ server/models/Review.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
-
+    reviewText: {
+        type: String,
+    },
 });
 
 const Review = mongoose.model('Review', reviewSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,47 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Review = require('./Review');
 
 const userSchema = new Schema({
-
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  reviews: [Review.schema]
 });
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
