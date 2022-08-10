3ef50241312ee4cfa26bc9949e972c5e16d555ba
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-10 17:05:31 -0400

    1200 px view complete

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -83,7 +83,7 @@ const SearchBooks = () => {
                   onChange={(e) => setSearchInput(e.target.value)}
                   type='text'
                   size='lg'
-                  placeholder='Search for a book'
+                  placeholder='Search for a book!'
                 />
               </Col>
               <Col xs={12} lg={1} md={2} sm={3} id='submit-button'>
