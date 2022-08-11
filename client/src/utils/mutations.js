7f0301ff4106ce4c9a86fbc63496a77009d2ee9d
tmenture <thomasoxemail@gmail.com>
2022-08-10 21:22:40 -0400

    fixed delete button

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -55,7 +55,7 @@ export const SAVE_BOOK = gql`
 
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
-    removeBook(bookId: $bookId) {
+    deleteBook(bookId: $bookId) {
       _id
       username
       email
